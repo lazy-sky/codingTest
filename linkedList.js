0840013f40987b6ceebefa5b2a851fe975e72f60
lazy-sky <1cielo@naver.com>
2021-12-10 22:53:50 +0900

    Linked list in JS

---
+++ linkedList.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  find(value) {
+    let currentNode = this.head;
+    while (currentNode.value !== value) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  append(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insert(node, newValue) {
+    const newNode = new Node(newValue);
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+
+  remove(value) {
+    let previousNode = this.head;
+    while (previousNode.next.value !== value) {
+      previousNode = previousNode.next;
+    }
+
+    if (previousNode.next !== null) {
+      previousNode.next = previousNode.next.next;
+    }
+  }
+
+  display() {
+    let currentNode = this.head;
+    let displayString = '[';
+    while (currentNode !== null) {
+      displayString += `${currentNode.value}, `;
+      currentNode = currentNode.next;
+    }
+    displayString = displayString.substring(0, displayString.length - 2);
+    displayString += ']';
+    console.log(displayString);
+  }
+}
+
+const linkedList = new SinglyLinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(5);
+linkedList.display();
+console.log(linkedList.find(3));
+linkedList.remove(3);
+linkedList.display();
+linkedList.insert(linkedList.find(2), 10);
+linkedList.display();

55a5947373eb1ce396124fea124d759c2bec0b72
lazy-sky <1cielo@naver.com>
2022-03-02 08:44:13 +0900

    큐 구현

---
+++ template/queue.js
@@ -0,0 +1,67 @@
+// 배열 버전
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+const queue = new Queue();
+
+// 연결 리스트 버전
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class QueueByLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size += 1;
+  }
+
+  dequeue() {
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size -= 1;
+    return value;
+  }
+
+  peek() {
+    return this.head.value;
+  }
+}
